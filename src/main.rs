4e480e11c61cbfb707bd49ec952f0054642ae4e2
Danial <dania99.dev@gmail.com>
2021-07-31 16:10:54 +0430

    listening to the TCP connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
