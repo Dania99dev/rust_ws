17e5e86565ed2ae734e89c77074508fc48c57d47
Danial <dania99.dev@gmail.com>
2021-08-03 16:34:58 +0430

    implementing the execute method

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,9 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -44,12 +47,15 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {} got a job; executing.", id);
+            job();
         });
 
         Worker { id, thread }
     }
 }
 
-struct Job;
+type Job = Box<dyn FnOnce() + Send + 'static>;
